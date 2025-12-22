dc5c3f92d8df042af1d447a4d4e6e0ab07ad8c67
sk4r <thibaut.frin@gmail.com>
2025-12-22 15:39:22 +0100

    update

---
+++ BINSEC/My_tests/souk/get_souk.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://github.com/Angelo942/FCSC2022/raw/main/Rev-souk-Easy/souk
+chmod +x souk
+gdb -x $SOUK_PATH/command --args ./souk
+$BINSEC_EXE -sse -sse-script $SOUK_PATH/crackme.ini -sse-depth 100000  -sse-qmerge 100 ./core.snapshot
